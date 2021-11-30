5ae4a0c03414b7ef3152f884ebda2f9847327e06
Will Worth <will.worth@gmail.com>
2021-11-30 19:41:50 +0100

    deploy script

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    // this variable is special because it's called a "state variable" 
+    // and it's cool because it's stored permanently in contract storage.
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender); // wallet address of the person who called the function
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
